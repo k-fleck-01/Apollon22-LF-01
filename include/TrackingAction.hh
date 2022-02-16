a32aa78dedd7edb89d634fd9fdde7338ad76fd33
Kyle Fleck <kfleck@Kyles-MacBook-Pro.local>
2022-02-16 02:57:25 +0000

    added tracking action

---
+++ include/TrackingAction.hh
@@ -0,0 +1,26 @@
+#ifndef _TRACKING_ACTION_H_
+#define _TRACKING_ACTION_H_
+// *
+// GEANT4 simulation of the Apollon 2022 experiment.
+// Geometry has been derived from the FLUKA simulation of the same experiment.
+// 
+// Header file for TrackingAction class
+// Last edited: 15/02/2022
+// *
+
+#include "G4UserTrackingAction.hh"
+
+class RunAction;
+class G4Track;
+
+class TrackingAction : public G4UserTrackingAction {
+    public:
+        TrackingAction(RunAction*);
+        ~TrackingAction();
+
+    public:
+        virtual void PreUserTrackingAction(const G4Track*);
+        virtual void PostUserTrackingAction(const G4Track*);
+};
+
+#endif
